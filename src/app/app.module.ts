5884d314bea77133cf698275f52d09ca0088a38c
Surya Kusjanto <skusjanto@outlook.com>
2023-09-20 16:15:21 -0600

    Add imports and providers to take care of redirects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { PathLocationStrategy, LocationStrategy } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BackgroundBubblesComponent } from './background-bubbles/background-bubbles.component';
@@ -8,9 +8,14 @@ import { ParticleComponent } from './particle/particle.component';
 import { ParticleExplosionComponent } from './particle-explosion/particle-explosion.component';
 
 @NgModule({
-  declarations: [AppComponent, BackgroundBubblesComponent, ParticleComponent, ParticleExplosionComponent],
+  declarations: [
+    AppComponent,
+    BackgroundBubblesComponent,
+    ParticleComponent,
+    ParticleExplosionComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  providers: [{ provide: LocationStrategy, useClass: PathLocationStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
